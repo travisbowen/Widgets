60287f1f78b7466773583ea530b3852afcdce058
Travis Bowen <Travisbowen23@gmail.com>
2020-08-13 20:14:49 -0700

    Added comments to the code

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-	const [term, setTerm] = useState("");
+	// Hooks useState for search term and search results
+	const [term, setTerm] = useState("programming");
+	const [results, setResults] = useState([]);
+
+	// Runs when re-rendered and when the term changes
+	useEffect(() => {
+		const search = async () => {
+			const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+				// params passed in for the wikipedia search link
+				params: {
+					action: "query",
+					list: "search",
+					origin: "*",
+					format: "json",
+					srsearch: term,
+				},
+			});
+			// Results from query
+			setResults(data.query.search);
+		};
+		search();
+		// Passed in term in array gets called each time the term changes
+	}, [term]);
+
+	// Maps through all results and creates jsx for each item then sets it to renderedResults
+	const renderedResults = results.map((result) => {
+		return (
+			<div key={result.pageid} className='item'>
+				<div className='content'>
+					<div className='header'>{result.title}</div>
+					<span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+				</div>
+			</div>
+		);
+	});
+
 	return (
 		<div>
 			<div className='ui form'>
@@ -14,6 +50,7 @@ const Search = () => {
 					/>
 				</div>
 			</div>
+			<div className='ui celled list'>{renderedResults}</div>
 		</div>
 	);
 };
